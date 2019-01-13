7fc674e969afadb0b1b3fb8fc890ef0670a05f6e
walk8243 <walk8243@gmail.com>
2019-01-13 19:25:42 +0900

    複数の申し込み番号を入力できるようにする

--- main/src/main/java/com/example/nobuyasu/ticketresultandroid/main/MainActivity.java
+++ main/src/main/java/com/example/nobuyasu/ticketresultandroid/main/MainActivity.java
@@ -8,36 +8,49 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     final HttpRequest HttpRequest = new HttpRequest();
+    LinearLayout receiptBox;
     EditText phoneNumber;
-    EditText receiptNumber;
-    TextView receiptResult;
+    List<EditText> receiptNumbers = new ArrayList<EditText>();
+    List<TextView> receiptResults = new ArrayList<TextView>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        receiptBox = (LinearLayout) findViewById(R.id.receipts);
         phoneNumber = (EditText) findViewById(R.id.phone_number);
-        receiptNumber = (EditText) findViewById(R.id.receipt1_number);
-        receiptResult = (TextView) findViewById(R.id.receipt1_result);
+        phoneNumber.setText("09000000000");
+
+        addReceipt("RT00000XXX00");
+        addReceipt("RT00000XXX00");
 
         findViewById(R.id.submit).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                reviewTicket();
+                if (phoneNumber.getText().length() == 0) {
+                    System.out.println("phoneNumber Error");
+                    return;
+                }
+                System.out.println(receiptNumbers.size());
+                for (int i=0; i<receiptNumbers.size(); ++i) {
+                    System.out.println(receiptNumbers.get(i).getText());
+                    System.out.println(receiptResults.get(i).getText());
+                    reviewTicket(i);
+                }
             }
         });
     }
 
-    protected void reviewTicket() {
-        if (phoneNumber.getText().length() == 0) {
-            System.out.println("phoneNumber Error");
-            return;
-        }
+    protected void reviewTicket(int index) {
+        EditText receiptNumber = receiptNumbers.get(index);
+        TextView receiptResult = receiptResults.get(index);
         if (receiptNumber.getText().length() == 0) {
             System.out.println("receiptNumber Error");
             return;
@@ -45,10 +58,10 @@ public class MainActivity extends AppCompatActivity {
         HashMap<String, String> reviewData = new HashMap<String, String>();
         reviewData.put("entry_no", receiptNumber.getText().toString());
         reviewData.put("tel_no", phoneNumber.getText().toString());
-        httpRequest("https://rt.tstar.jp/lots/review", "POST", reviewData);
+        httpRequest("https://rt.tstar.jp/lots/review", "POST", reviewData, receiptResult);
     }
 
-    protected void httpRequest(final String urlStr, final String method, final HashMap<String, String> data) {
+    protected void httpRequest(final String urlStr, final String method, final HashMap<String, String> data, final TextView target) {
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -67,19 +80,39 @@ public class MainActivity extends AppCompatActivity {
 
                 if (result.indexOf("抽選の結果、お客様はご当選されました。") > -1) {
                     System.out.println("当選");
-                    receiptResult.setText("当選");
+                    target.setText("当選");
                 } else if (result.indexOf("抽選の結果、お客様は残念ながら落選となりました。") > -1) {
                     System.out.println("落選");
-                    receiptResult.setText("落選");
+                    target.setText("落選");
                 } else if (result.indexOf("抽選結果発表") > -1) {
                     System.out.println("抽選前");
-                    receiptResult.setText("抽選前");
+                    target.setText("抽選前");
                 } else {
 //                    System.out.println(result);
                     System.out.println("抽選申し込み番号または電話番号が違います。");
-                    receiptResult.setText("抽選申し込み番号または電話番号が違います。");
+                    target.setText("抽選申し込み番号または電話番号が違います。");
                 }
             }
         }).start();
+    }
+
+    protected void addReceipt(String receiptNum) {
+        LinearLayout receiptAdd = new LinearLayout(findViewById(R.id.receipts).getContext());
+        receiptAdd.setOrientation(LinearLayout.VERTICAL);
+        receiptAdd.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        receiptBox.addView(receiptAdd);
+
+        EditText receiptNumberAdd = new EditText(receiptAdd.getContext());
+        receiptNumberAdd.setText(receiptNum);
+        receiptNumberAdd.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        receiptAdd.addView(receiptNumberAdd);
+
+        TextView receiptResultAdd = new TextView(receiptAdd.getContext());
+//        receiptResultAdd.setText("ddd");
+        receiptResultAdd.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        receiptAdd.addView(receiptResultAdd);
+
+        receiptNumbers.add(receiptNumberAdd);
+        receiptResults.add(receiptResultAdd);
     }
 }
