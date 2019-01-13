bb0fe0946068bdcbda5b9f471e54938f886ad6e0
walk8243 <walk8243@gmail.com>
2019-01-13 16:02:11 +0900

    単体のチケットの抽選結果を確認

--- main/src/main/java/com/example/nobuyasu/ticketresultandroid/main/HttpRequest.java
+++ main/src/main/java/com/example/nobuyasu/ticketresultandroid/main/HttpRequest.java
@@ -4,33 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
 
 public class HttpRequest {
-    protected void httpRequest(final String urlStr, final String method) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                String result = new String();
-                try {
-                    URL url = new URL(urlStr);
-                    if (method == "GET") {
-                        result = getRequest(url);
-                    } else if (method == "POST") {
-                        result = postRequest(url);
-                    }
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-
-                System.out.println(result);
-            }
-        }).start();
-    }
-
-    protected String getRequest(final URL url) throws Exception {
+    protected String getRequest(final URL url, final HashMap<String, String> data) throws Exception {
         try {
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("GET");
@@ -52,7 +32,6 @@ public class HttpRequest {
                 inReader.close();
                 in.close();
 
-                System.out.println(result.length());
                 return result;
             }
         } catch (Exception ex) {
@@ -63,14 +42,24 @@ public class HttpRequest {
         return new String();
     }
 
-    protected String postRequest(final URL url) throws Exception {
+    protected String postRequest(final URL url, final HashMap<String, String> data) throws Exception {
         try {
+//            System.out.println(url.toString());
             HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
             urlConnection.setRequestMethod("POST");
             urlConnection.connect();
 
-            int statusCode = urlConnection.getResponseCode();
+            String postParam = new String();
+            for (String key : data.keySet()) {
+                postParam = postParam.concat("&"+key+"="+data.get(key));
+            }
+//            System.out.println(postParam.substring(1));
+            OutputStream outputStream = null;
+            outputStream = urlConnection.getOutputStream();
+            outputStream.write(postParam.substring(1).getBytes("UTF-8"));
+            outputStream.flush();
 
+            int statusCode = urlConnection.getResponseCode();
             if(statusCode == HttpURLConnection.HTTP_OK){
                 String result = new String();
                 //responseの読み込み
@@ -79,7 +68,10 @@ public class HttpRequest {
                 final BufferedReader bufferedReader = new BufferedReader(inReader);
                 String line = null;
                 while((line = bufferedReader.readLine()) != null) {
-                    result.concat(line);
+                    result = result.concat(line);
+                }
+                if(outputStream != null) {
+                    outputStream.close();
                 }
                 bufferedReader.close();
                 inReader.close();
--- main/src/main/java/com/example/nobuyasu/ticketresultandroid/main/MainActivity.java
+++ main/src/main/java/com/example/nobuyasu/ticketresultandroid/main/MainActivity.java
@@ -3,27 +3,83 @@ package com.example.nobuyasu.ticketresultandroid.main;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import com.example.nobuyasu.ticketresultandroid.main.HttpRequest;
-
+import java.net.URL;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    final HttpRequest HttpRequest = new HttpRequest();
+    EditText phoneNumber;
+    EditText receiptNumber;
+    TextView receiptResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final HttpRequest HttpRequest = new HttpRequest();
+        phoneNumber = (EditText) findViewById(R.id.phone_number);
+        receiptNumber = (EditText) findViewById(R.id.receipt1_number);
+        receiptResult = (TextView) findViewById(R.id.receipt1_result);
 
-        findViewById(R.id.testMethod).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.submit).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                HttpRequest.httpRequest("https://qiita.com/walk8243/items/9bf842425414a8cd5ead", "GET");
-//                HttpRequest.httpRequest("https://rt.tstar.jp/lots/review", "GET");
-//                HttpRequest.httpRequest("https://rt.tstar.jp/lots/review", "POST");
+                reviewTicket();
             }
         });
+    }
+
+    protected void reviewTicket() {
+        if (phoneNumber.getText().length() == 0) {
+            System.out.println("phoneNumber Error");
+            return;
+        }
+        if (receiptNumber.getText().length() == 0) {
+            System.out.println("receiptNumber Error");
+            return;
+        }
+        HashMap<String, String> reviewData = new HashMap<String, String>();
+        reviewData.put("entry_no", receiptNumber.getText().toString());
+        reviewData.put("tel_no", phoneNumber.getText().toString());
+        httpRequest("https://rt.tstar.jp/lots/review", "POST", reviewData);
+    }
+
+    protected void httpRequest(final String urlStr, final String method, final HashMap<String, String> data) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String result = new String();
+                try {
+                    URL url = new URL(urlStr);
+                    if (method == "GET") {
+                        result = HttpRequest.getRequest(url, data);
+                    } else if (method == "POST") {
+                        result = HttpRequest.postRequest(url, data);
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                    return;
+                }
+
+                if (result.indexOf("抽選の結果、お客様はご当選されました。") > -1) {
+                    System.out.println("当選");
+                    receiptResult.setText("当選");
+                } else if (result.indexOf("抽選の結果、お客様は残念ながら落選となりました。") > -1) {
+                    System.out.println("落選");
+                    receiptResult.setText("落選");
+                } else if (result.indexOf("抽選結果発表") > -1) {
+                    System.out.println("抽選前");
+                    receiptResult.setText("抽選前");
+                } else {
+//                    System.out.println(result);
+                    System.out.println("抽選申し込み番号または電話番号が違います。");
+                    receiptResult.setText("抽選申し込み番号または電話番号が違います。");
+                }
+            }
+        }).start();
     }
 }
